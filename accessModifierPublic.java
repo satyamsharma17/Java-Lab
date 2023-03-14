96cf96199e67df7b83a4e4a5cdb49ec3ac1e6f79
Satyam Sharma <satyamsharma1725@gmail.com>
2023-03-14 15:31:59 +0530

    Add files via upload

---
+++ accessModifierPublic.java
@@ -0,0 +1,9 @@
+public class accessModifierPublic {
+    private static void display() {
+        System.out.println("Hello geeks!");
+    }
+
+    public static void main(String args[]) {
+        display();
+    }
+}
